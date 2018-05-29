6978cbdc678b640cfa0543de0fd64d717b74ca4d
Dojun LEE <leedoe0709@gmail.com>
2018-05-30 00:09:42 +0900

    다이어리 1차

--- src/components/diary.js
+++ src/components/diary.js
@@ -2,26 +2,35 @@ import React, { Component } from 'react';
 import './diary.css';
 
 class Diary extends Component {
-    render() {
-        return(
-            <div className='content'>
-                <span>나의 이더리움 지갑 주소</span><br/>
-                <span>........</span>
+  render() {
+    return (
+      <div className='content left'>
+        <span class='etherAaddress'>나의 이더리움 지갑 주소</span><br />
+        <span>........</span>
 
-                <div class='diary'>
-                    <div class='token'>
-                        <span class='title'>2018 지방선거 참여자</span>
-                        <div class='token'>
-                        </div>
-                    </div>
-                    <div>
-                    </div>
-                    <div>
-                    </div>
-                </div>
+        <div class='diary'>
+          <div class='card'>
+            <span class='title'>2018 지방선거 참여자</span>
+            <div class='token'>
+              토큰 이미지
             </div>
-        )
-    }
+          </div>
+          <div class='card'>
+            <span class='title'>2018 지방선거 참여자</span>
+            <div class='token'>
+              토큰 이미지
+          </div>
+          </div>
+          <div class='card'>
+            <span class='title'>2018 지방선거 참여자</span>
+            <div class='token'>
+              토큰 이미지
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Diary
