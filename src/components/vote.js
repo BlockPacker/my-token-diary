421fdaff27555a43c718b632fe68a3b325f98624
DOJUN <>
2018-05-31 17:11:24 +0900

    firebase 통신 추가

--- src/components/vote.js
+++ src/components/vote.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
 import './vote.css'
 
-// const firebase = require('firebase');
-// require('firebase/firestore');
-
-// firebase.initializeApp({
-//     apiKey: 'AIzaSyAqF_ts74PnuemldvOJVoYaGb0oelIKmxM',
-//     authDomain: 'mytokendiray.firebaseapp.com',
-//     projectId: 'mytokendiray',
-// });
-
-// const db = firebase.firestore();
+firebase.initializeApp({
+    apiKey: 'AIzaSyAqF_ts74PnuemldvOJVoYaGb0oelIKmxM',
+    authDomain: "mytokendiray.firebaseapp.com",
+    databaseURL: "https://mytokendiray.firebaseio.com",
+    projectId: "mytokendiray",
+    storageBucket: "mytokendiray.appspot.com",
+    messagingSenderId: "403467372211"
+});
+const db = firebase.firestore();
 
 class Vote extends Component {
     constructor(props) {
@@ -21,14 +24,31 @@ class Vote extends Component {
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEthChange = this.handleEthChange.bind(this);
+        this.handleVoteChange = this.handleVoteChange.bind(this);
     }
 
-    handleChange(event) {
-        console.log(event);
+    handleEthChange(event) {
+        this.setState({etherAddress: event.target.value});
+    }
+
+    handleVoteChange(event) {
+        this.setState({voteUrl: event.target.value});
     }
     
     handleSubmit(event) {
-        alert('Values: ' + this.state.value);
+        alert('eth: ' + this.state.etherAddress + '\nvote: ' + this.state.voteUrl);
+        db.collection('vote').add({
+            ether_address: this.state.etherAddress,
+            user: null,
+            vote_url: this.state.voteUrl,
+            insert_time: Date.now(),
+            done: false
+        }).then(function(docRef) {
+            console.log('Document written with ID: ', docRef.id);
+        }).catch(function(error){
+            console.error('Error adding document: ', error);
+        });
         event.preventDefault();
     }
 
@@ -39,8 +59,8 @@ class Vote extends Component {
 
                 <form onSubmit={this.handleSubmit}>
                     {/* 이더리움 지갑 주소 로그인<br/> */}
-                    <input type='text' value={this.state.etherAddress} placeholder='이더리움 지갑 주소를 적어주세요'/>
-                    <input type='text' value={this.state.voteUrl} placeholder='인증 url을 적어주세요.'/>
+                    <input type='text' value={this.state.etherAddress} onChange={this.handleEthChange} placeholder='이더리움 지갑 주소를 적어주세요'/>
+                    <input type='text' value={this.state.voteUrl} onChange={this.handleVoteChange} placeholder='인증 url을 적어주세요.'/>
                     <input type='submit'/>
                 </form>
             </div>
